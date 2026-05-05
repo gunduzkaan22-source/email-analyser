cd15f27cbbb276036da40cdc1d98efb7266e5c73
Kaan Gunduz <gunduzkaan22@gmail.com>
2026-05-06 00:11:39 +0100

    Fix port to 8080

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def analyse():
     return jsonify(json.loads(cleaned))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
